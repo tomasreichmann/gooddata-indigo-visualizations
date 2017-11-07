d5490c7065cfb464e24a76bb1c0bd239c8ec5e33
David Ocetnik <david.ocetnik@gooddata.com>
2017-11-07 13:03:33 +0100

    Rename exception to error in tests

--- src/Table/utils/test/dataTransformation.spec.js
+++ src/Table/utils/test/dataTransformation.spec.js
@@ -156,10 +156,10 @@ describe('Table utils - Data transformation', () => {
         });
     });
 
-    describe('Exceptions', () => {
+    describe('Errors', () => {
         const errorMessage = 'Number of dimensions must be equal two';
 
-        it('should throw exception if number of dimensions is equal zero', () => {
+        it('should throw error if number of dimensions is equal zero', () => {
             expect(() => {
                 getHeaders({
                     dimensions: []
@@ -167,7 +167,7 @@ describe('Table utils - Data transformation', () => {
             }).toThrow(errorMessage);
         });
 
-        it('should throw exception if number of dimensions is equal one', () => {
+        it('should throw error if number of dimensions is equal one', () => {
             expect(() => {
                 getHeaders({
                     dimensions: [
@@ -177,7 +177,7 @@ describe('Table utils - Data transformation', () => {
             }).toThrow(errorMessage);
         });
 
-        it('should throw exception if number of dimensions is equal three', () => {
+        it('should throw error if number of dimensions is equal three', () => {
             expect(() => {
                 getHeaders({
                     dimensions: [
@@ -388,19 +388,19 @@ describe('Table utils - Data transformation', () => {
     describe('Validate table proportions', () => {
         const errorMessage = 'Number of table columns must be equal to number of table headers';
 
-        it('should not throw exception if number of table columns is equal zero', () => {
+        it('should not throw error if number of table columns is equal zero', () => {
             expect(() => {
                 validateTableProportions(TABLE_HEADERS_1A_2M, []);
             }).not.toThrow(errorMessage);
         });
 
-        it('should not throw exception if number of table columns is equal to number of table headers', () => {
+        it('should not throw error if number of table columns is equal to number of table headers', () => {
             expect(() => {
                 validateTableProportions(TABLE_HEADERS_1A_2M, TABLE_ROWS_1A_2M);
             }).not.toThrow(errorMessage);
         });
 
-        it('should throw exception if number of table columns is not equal to number of table headers', () => {
+        it('should throw error if number of table columns is not equal to number of table headers', () => {
             expect(() => {
                 validateTableProportions(TABLE_HEADERS_1A_2M, TABLE_ROWS_1A);
             }).toThrow(errorMessage);
